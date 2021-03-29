e73b5139d39b6f83f6872c99a0e565224d9081af
witusj <wfth69@gmail.com>
2021-03-29 13:16:42 +0200

    corrected error

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import styles from '../styles/Toolbar.module.scss'
+import styles from '../styles/toolbar.module.scss'
 
 export const Toolbar = () => {
     const router = useRouter()

1c5f979de4e9fab84df1002e118f010b61353a6b
witusj <wfth69@gmail.com>
2021-03-30 08:22:13 +0200

    Minor change

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,8 +8,8 @@ export const Toolbar = () => {
         <div>
             <img className="img-fluid" src="logo.png"></img>
             <div className={styles.main}>
-                <div className="lead" onClick={() => router.push('/')}>Home</div>
-                <div className="lead" onClick={() => router.push('/inspiration')}>Inspiration</div>
+                <div className="lead" onClick={() => router.push('/')}>Start</div>
+                <div className="lead" onClick={() => router.push('/inspiration')}>Inspiratie</div>
                 <div className="lead" onClick={() => window.open('https://www.linkedin.com/in/witektenhove/', '_blank')}>Linkedin</div>
             </div>
         </div>

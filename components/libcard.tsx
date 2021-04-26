2108bc2c91b44e6058fbec5e91b4b80d51614565
witusj <wfth69@gmail.com>
2021-04-26 09:09:06 +0200

    Deleted classNames

--- components/libcard.tsx
+++ components/libcard.tsx
@@ -2,16 +2,16 @@ import { Button, Card, Col } from 'react-bootstrap'
 
 export const LibCard = (props) => {
     return (
-        <div className={styles.main}>
+        <div>
             <Col md={4}>
-                <Card className={styles.inspirationcard}>
+                <Card>
                     <Card.Img variant="top" src={props.imagesrc} />
-                        <Card.Body className={styles.inspirationbody}>
-                        <Card.Title className={styles.inspirationtitle}>{ props.title }</Card.Title>
+                        <Card.Body>
+                        <Card.Title>{ props.title }</Card.Title>
                             <Card.Text>
                                 Gepubliceerd op: { props.description }
                             </Card.Text>
-                        <Button variant="primary">Naar { props.title }</Button>
+                        <Button>Naar { props.title }</Button>
                     </Card.Body>
                 </Card>
             </Col>

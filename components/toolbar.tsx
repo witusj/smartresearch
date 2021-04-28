82c6c6de8443e7d9810fca64dcf036fbe7feb11a
witusj <wfth69@gmail.com>
2021-04-28 15:55:16 +0200

    Added video content

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,19 +8,18 @@ export const Toolbar = () => {
         <div className="mb-5">
             <img className="img-fluid mb-5" src="logo.png"></img>
             <Navbar expand="lg">
-            <div className="mx-auto d-sm-flex d-block flex-sm-nowrap">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link className="lead" onClick={() => router.push('/')}>Start</Nav.Link>
-                <Nav.Link className="lead" onClick={() => router.push('/inspiration')}>Inspiratie</Nav.Link>
-                <Nav.Link className="lead" onClick={() => router.push('/libguides')}>Libguides</Nav.Link>
-                <Nav.Link className="lead" onClick={() => window.open('https://www.linkedin.com/in/witektenhove/', '_blank')}>Linkedin</Nav.Link>
-                </Nav>
-                    </Navbar.Collapse>
-                    </div>
+                <div className="mx-auto d-sm-flex d-block flex-sm-nowrap">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link className="lead" onClick={() => router.push('/')}>Start</Nav.Link>
+                    <Nav.Link className="lead" onClick={() => router.push('/inspiration')}>Inspiratie</Nav.Link>
+                    <Nav.Link className="lead" onClick={() => router.push('/libguides')}>Libguides</Nav.Link>
+                    <Nav.Link className="lead" onClick={() => window.open('https://www.linkedin.com/in/witektenhove/', '_blank')}>Linkedin</Nav.Link>
+                    </Nav>
+                        </Navbar.Collapse>
+                </div>
             </Navbar>
-                
-            </div>
+        </div>
     )
 }

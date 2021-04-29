05a088f2c7f8af800848d6f1373f1dfcaabbf568
witusj <wfth69@gmail.com>
2021-04-29 12:46:30 +0200

    Added padding to toolbar

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -12,9 +12,9 @@ export const Toolbar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link className="lead" onClick={() => router.push('/')}>Start</Nav.Link>
-                    <Nav.Link className="lead" onClick={() => router.push('/inspiration')}>Inspiratie</Nav.Link>
-                    <Nav.Link className="lead" onClick={() => router.push('/libguides')}>Libguides</Nav.Link>
+                    <Nav.Link className="lead pr-5" onClick={() => router.push('/')}>Start</Nav.Link>
+                    <Nav.Link className="lead pr-5" onClick={() => router.push('/inspiration')}>Inspiratie</Nav.Link>
+                    <Nav.Link className="lead pr-5" onClick={() => router.push('/libguides')}>Libguides</Nav.Link>
                     <Nav.Link className="lead" onClick={() => window.open('https://www.linkedin.com/in/witektenhove/', '_blank')}>Linkedin</Nav.Link>
                     </Nav>
                         </Navbar.Collapse>

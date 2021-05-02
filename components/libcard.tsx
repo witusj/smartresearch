43311ab13813ef1d3bca89e35d8e6f9bccbbb86c
witusj <wfth69@gmail.com>
2021-05-02 14:12:21 +0200

    Added gtag event for libguides

--- components/libcard.tsx
+++ components/libcard.tsx
@@ -1,7 +1,20 @@
 import { Button, Card, Col } from 'react-bootstrap'
 import styles from '../styles/libcard.module.scss'
+import * as gtag from '../lib/gtag.js'
+import { propTypes } from 'react-bootstrap/esm/Image'
+
+
 
 export const LibCard = (props) => {
+    const toLibguide = ()=>  {
+    gtag.event ({
+        action : "choose_libguide",
+        category : `${ props.title }`,
+        label : "libguide chosen",
+        value : "done"
+    })
+    }
+    
     return (
             <Col md={4}>
             <Card className={styles.libcardcard}>
@@ -12,7 +25,7 @@ export const LibCard = (props) => {
                             <Card.Text>
                                 { props.description }
                             </Card.Text>
-                        <Button className="libguides" href={`./libs?cid=${ props.cid }&title=${ props.title }`}>Naar { props.title }</Button>
+                    <Button className="libguides" href={`./libs?cid=${props.cid}&title=${props.title}`} onClick={() => toLibguide()} >Naar { props.title }</Button>
                     </Card.Body>
                 </Card>
             </Col>

0f379b34c702de9b36e908c7874b639c73c8d5e5
witusj <wfth69@gmail.com>
2021-05-05 09:52:18 +0200

    deleted gtag from app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../paused_lib/gtag'
 
 const App = ({ Component, pageProps }) => {
 

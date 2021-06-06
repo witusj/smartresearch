5c2a1b1224befdf562d96647dc541fe9c383a13b
witusj <wfth69@gmail.com>
2021-06-06 12:00:43 +0200

    Added open graph to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,14 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <title>Smart Research &#127891;</title>
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <meta name="description" content="Tools and methods for doing Smart Research. Audit you research process, discover new research tools and methods and find interesting material in the selected libguides."></meta>
+                    <meta property="og:title" content="Smart Research &#127891;" />
+                    <meta property='og:description' content="Tools and methods for doing Smart Research. Audit you research process, discover new research tools and methods and find interesting material in the selected libguides."/>
+                    <meta property="og:type" content="website" />
+                    <meta property="og:image" content="smartresearch.png" />
+                    <meta property="og:url" content="https://smartresearch.vercel.app" />
                 </Head>
                 <body>
                     <Main />

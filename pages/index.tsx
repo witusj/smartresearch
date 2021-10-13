9436d6c764ae8c58e22a0f828012ecd302a5e7b2
witusj <wfth69@gmail.com>
2021-10-13 14:53:22 +0200

    Minor

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,7 +134,7 @@ const Home = () => {
       <div className={styles.main}>
         <Intro
           heading="Kwaliteitsnormen Smart Research"
-          lead="Het doel van onderzoek is waardevolle kennis te genereren m.b.t. complexe vraagstukken en duurzame oplossingsrichtingen. We definiëren voor deze context onderzoek als het gehele proces van genereren, delen en verwerken van kennis. Door studenten
+          lead="Het doel van onderzoek is waardevolle kennis te genereren m.b.t. complexe vraagstukken en duurzame oplossingsrichtingen. We definiëren voor deze context onderzoek als het gehele proces van genereren, delen en verwerken van kennis en informatie. Door studenten
             van te voren goed te laten nadenken over dit proces en te ondersteunen bij de inrichting ervan wordt gestreefd naar een adequate kwaliteit van uitvoering en uitkomsten. De informatie op deze website dient ter inspiratie voor zowel docenten als studenten bij het ontwerp en uitvoering van onderzoek en geeft aanknopingspunten voor feedback en assessment."
         />
         <Paragraph
